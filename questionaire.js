dbaf68b84cc0d7f8d276ad8935a6731184eed121
Recelis <jackymlui@live.com>
2017-05-22 16:43:16 +1000

    javascript running

---
+++ questionaire.js
@@ -0,0 +1,56 @@
+
+var data = {
+    counter:0,
+    questions:['are you sure?','are you ready to begin?', 'do you like dogs?', 'do you like cats?'],
+    changeCounter:function(id){
+        if (id === 'yes'){
+            data.counter++;
+            if (data.counter > data.questions.length -1) return data.counter = 0;
+            return data.counter;
+        } else{
+            data.counter--;
+            if (data.counter < 0) return data.counter = data.questions.length -1;
+            return data.counter;
+        }
+    }
+};
+
+var handler = {
+    // button listeners
+    buttonCreateListeners: function(id){
+        document.getElementById('no').addEventListener("click",handler.changeQuestion(id));
+    },
+    changeQuestion:function(id){
+        data.changeCounter(id);
+        view.updateText();// update view
+    }
+};
+
+var view = {
+    // create buttons
+    createButtons: function(id, parent){ 
+        var newButton = document.createElement('button');
+        newButton.id = id;
+        newButton.textContent = id; 
+        document.getElementById(parent).appendChild(newButton);
+        newButton.onclick = function(){
+            handler.changeQuestion(id);
+        };
+        return id;
+    },
+    updateText: function(){
+        document.getElementById('textContent').innerHTML = data.questions[data.counter]; // this hardcoded textcontent stinks
+    }
+
+};
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    // add buttons to placeButton
+    view.createButtons('yes', 'placeButton');
+    view.createButtons('no', 'placeButton');
+    // create listeners for buttons
+    // handler.buttonCreateListeners('yes');
+    
+});
+
