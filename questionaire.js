f26c2b6187a9f2463c414a901a41625f8d7181db
Recelis <jackymlui@live.com>
2017-05-23 19:55:13 +1000

    some answers written up

--- questionaire.js
+++ questionaire.js
@@ -12,6 +12,8 @@ var data = {
         'does your diary or to-do-list reflect your long-term plan?', 
         'did you break down a big complicated job so that you would avoid procrastination?',
         'did you avoid procrastinating on a scary task?',
+        'did you get through a setback?',
+        'did you take a step back from a setback and look at a bigger picture?',
         'are you simplified so that you don\'t too much on your plate?',
         'did you do at least 4 hours of work before taking a break today?',
         'have you got rid of easily accessible temptations?',
@@ -28,12 +30,27 @@ var data = {
         'did you go to sleep early last night?',
         'do you feel grateful to anyone?'],
     answers:{
-        no:'change your no to a yes and carry on. Ready to move on?',
+        no:'Change your no to a yes and carry on. Ready to move on?',
         
     },
     suggestions:{
         advice: [
-
+            'make your bed so that you start off the day having completed one thing well',
+            'having your desk neat and tidy means that you won\'t be easily distracted and you will be more focussed',
+            'when you have a plan for the day, you\'ll easily focus and know exactly what you have to do',
+            'when you have plan for tomorrow, you will have a purpose for the next day, you will have anticipation and jump out of bed ready to conquer the world.',
+            'when your calendar is organised, you will plan and be able to go to your events right on time, getting the most out of that event',
+            'when you have a long-term plan, then you strive through life with purpose and confidence.',
+            'when your daily diary or to-do list reflects your long term plan, then your execution of your plan will be achieved',
+            'when you break down a big complicated job into smaller manageable tasks, you will be able easily get through the big problems',
+            'when you get stuck on a task that requires heaps of courage, just know that these are challenges that help you grow to become more like a person who can conquer anything.',
+            'when you carry through on a setback, you build resilience. A great characteristic to have!',
+            'sometimes when you have a setback, it\'s important to take a step back and look for solutions in a neighbouring field. Sometimes solutions are just another field away.',
+            'when you focus on a few things, you can get a lot done.',
+            'try to maximise your achievements of the day before you take your first break. Breaks are times when you can get distracted and lose momentum. Try to do a fairly big amount of work before you take that first break.',
+            'Distractions are everywhere, if you can clear out threats of distractions. Blocking out youtube, wiping your caches, you will be less inclined to be tempted to waste time. Life is pretty short.',
+            'Your health is extremely important, and eating well is a major part in living healthily. Stick away from junk food, high fat foods and sweets. Try to eat as much vegetables as possible',
+            '',
         ],
         last: 'looks like you are on the right track! Keep moving forward, and if you run into any difficulties, just come back here!',
     },
@@ -49,8 +66,8 @@ var data = {
         } else{
             data.noCounts++;
             data.counter--; // so that you bounce back to where you were
-            console.log(data.counter);
-            return data.answers.no;
+            
+            return data.suggestions.advice[data.counter] +' '+ data.answers.no;
         }
     }
 };
